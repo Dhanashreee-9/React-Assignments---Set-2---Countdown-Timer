929f0fda4a2979f6bba6551b82fd935f0fc2dbc4
Dhanashreee-9 <68267346+Dhanashreee-9@users.noreply.github.com>
2021-07-07 19:24:51 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  let [count,setCount] = useState()
+  
+  let increaseCount =(event)=>{
+    setCount(event.target.value)
+  }
+
+  useEffect(() => {
+    
+    if(count == '0' || count == "" || count<=0 || isNaN(count)){
+                setCount(0)
+              }else{
+                let x = setInterval(()=>{
+                  setCount(count - 1)
+                },1000)
+                return()=> clearInterval(x)
+              }
+
+  }, [count])
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={increaseCount} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{count}</div>
     </div>
   )
 }
